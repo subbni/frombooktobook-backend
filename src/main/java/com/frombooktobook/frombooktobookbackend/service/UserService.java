38de8de3d797faee03b8df880cf3c90b45422426
subbni <slfkak@naver.com>
2023-02-10 14:21:16 +0900

    #13- 임시비밀번호 발급 logic
    요청이 들어온 이메일이 가입된 이메일이라면 임시비밀번호로 비밀번호를 바꾼 뒤 메일로 전송

--- src/main/java/com/frombooktobook/frombooktobookbackend/service/UserService.java
+++ src/main/java/com/frombooktobook/frombooktobookbackend/service/UserService.java
@@ -63,25 +63,11 @@ public class UserService {
     public String changePasswordToTempPassword(User user) {
         String tempPassword;
         tempPassword = createRandomCode(8);
-        user.setPassword(passwordEncoder.encode(tempPassword));
-        return tempPassword;
     }
 
-//    @Transactional
-//    public void changePasswordToNewPassword(PasswordChangeRequestDto requestDto) {
-//        User user = findByEmail(requestDto.getEmail());
-//        if(checkIfPasswordIsCorrect(user,requestDto.getCurrentPassword())) {
-//            user.setPassword(passwordEncoder.encode(requestDto.getNewPassword()));
-//        } else {
-//            throw new WrongPasswordException("password wrong.");
-//        }
-//    }
-
-
-
-
-    public String createRandomCode(int length) {
-        return UUID.randomUUID().toString().substring(0,length);
+    @Transactional
+    public void changePassword(String email, String newPassword) throws Exception{
+        User user = findByEmail(email);
+        user.setPassword(passwordEncoder.encode(newPassword));
     }
-
 }
